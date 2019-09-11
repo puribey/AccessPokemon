c2eb1b16e533ad13c3eac827c518cf499b437c9d
puribey <kenshipichu@gmail.com>
2019-09-10 21:15:52 -0300

    client ready to start with designs

---
+++ client/src/components/PokemonList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="hello">
+    <h1>Bienvenidos al Accesible mundo de Pokemon</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PokemonList',
+  props: {
+  }
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
