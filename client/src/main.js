4425fc0f29a53e0ee2b6caa373fc3d010e2ad7d4
puribey <kenshipichu@gmail.com>
2019-09-13 09:42:07 -0300

    comments in one line

--- client/src/main.js
+++ client/src/main.js
@@ -4,6 +4,8 @@ import router from "./router";
 
 Vue.config.productionTip = false;
 
+// tabIndex should only be 0
+// It is a really bad practice to use positive numbers but this is for demo purposes
 Vue.directive("tab",{ bind: function (el, binding) {
     el.tabIndex = !binding ? "0" : binding.arg.toString();
 }});
